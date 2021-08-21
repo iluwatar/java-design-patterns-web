0d65edba37c37f6505d9016295b8743476df7d63
Ilkka Seppälä <ilkka.seppala@gofore.com>
2021-08-21 17:20:06 +0300

    https://github.com/iluwatar/java-design-patterns/issues/1807 Remove the localizations in the build process since they are not supported by this web site

--- cibuild.sh
+++ cibuild.sh
@@ -3,6 +3,7 @@
 # clone the repositories we need
 git clone https://github.com/iluwatar/30-seconds-of-java.git
 git clone https://github.com/iluwatar/java-design-patterns.git patterns
+rm -rf patterns/localization
 git clone https://github.com/iluwatar/programming-principles.git
 
 # add the currently used commit hash to the _config.yml so it can be displayed on the website
